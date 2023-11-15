395913823e5e83cbdcf15fae10869ae621f6a1c6
Boaz Pash <boazpash123@gmail.com>
2023-11-15 10:05:01 +0200

    adding try & catch

--- src/products/productsDal.ts
+++ src/products/productsDal.ts
@@ -1,15 +1,19 @@
 import { Categories } from "./productModel";
 
 export const getCategoriesDal = async () => {
-  const categories = await Categories.find({});
-  console.log(categories);
-
-  return categories;
+  try {
+    const categories = await Categories.find({});
+    console.log(categories);
+    return categories;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const topCategoriesDal = async () => {
-  const categories = await Categories.find().sort({ view: -1 }).limit(5);
-  console.log(categories);
-
-  return categories;
+  try {
+    const categories = await Categories.find().sort({ view: -1 }).limit(5);
+    console.log(categories);
+    return categories;
+  } catch (error) {}
 };
