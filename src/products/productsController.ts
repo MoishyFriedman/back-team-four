7b9035954235c879bec6aed9682d23e229a957c6
Boaz Pash <boazpash123@gmail.com>
2023-11-16 16:04:30 +0200

    ?

--- src/products/productsController.ts
+++ src/products/productsController.ts
@@ -3,11 +3,7 @@ import {
   getCategoriesService,
   topCategoriesService,
   getProductsService,
-<<<<<<< HEAD
   getTop5ProductsService
-=======
-  // addToCartService,
->>>>>>> main
 } from "./productsService";
 import { IProduct } from "../types";
 
--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(cors());
 app.use(morgan(`tiny`));
-app.use(express.json());
+app.use(express.json());             
 app.use(`/users`, usersRouter);
 app.use(`/products`, productsRouter);
 
